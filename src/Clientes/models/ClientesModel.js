f5c974acd9db03ca96bb23d190b00ecb8d7edec9
Ytallo Bruno <ytallobruno@hotmail.com>
2022-04-27 19:27:16 -0300

    feat: adicionado ClientesModel

---
+++ src/Clientes/models/ClientesModel.js
@@ -0,0 +1,10 @@
+class ClientesModel {
+  constructor({ id_comanda, id_pedido, nome_cliente, email_cliente }) {
+    this.id_comanda = id_comanda;
+    this.id_pedido = id_pedido;
+    this.nome_cliente = nome_cliente;
+    this.email_cliente = email_cliente;
+  }
+}
+
+export default ClientesModel;
