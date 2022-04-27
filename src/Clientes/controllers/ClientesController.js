252a0b77ba1feac4a78d14986f8a5d7fe0ddd8fa
Ytallo Bruno <ytallobruno@hotmail.com>
2022-04-27 19:27:06 -0300

    feat: adicionado ClientesController

---
+++ src/Clientes/controllers/ClientesController.js
@@ -0,0 +1,42 @@
+import ClientesTable from "../CriaCliente.js";
+import ClientesModel from "../models/ClientesModel.js";
+
+class ClientesController {
+  constructor({ id_comanda, id_pedido, nome_cliente, email_cliente }) {
+    this.id_comanda = id_comanda;
+    this.id_pedido = id_pedido;
+    this.nome_cliente = nome_cliente;
+    this.email_cliente = email_cliente;
+  }
+
+  static listar() {
+    return ClientesTable.findAll();
+  }
+
+  static listarUmItemPorId(req) {
+    return ClientesTable.findByPk(req.params.id);
+  }
+
+  adicionar() {
+    const cliente = new ClientesModel(this);
+    return ClientesTable.create(cliente);
+  }
+
+  static deletar(idDeletado) {
+    ClientesTable.destroy({
+      where: {
+        id: idDeletado,
+      },
+    });
+  }
+
+  static update(req) {
+    ClientesTable.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+  }
+}
+
+export default ClientesController;
