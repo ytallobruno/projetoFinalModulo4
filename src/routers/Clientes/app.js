73d22b333b0708bd8827b89befde4b998e894a01
Ytallo Bruno <ytallobruno@hotmail.com>
2022-05-01 12:06:02 -0300

    feat: código descomentado

--- src/routers/Clientes/app.js
+++ src/routers/Clientes/app.js
@@ -20,49 +20,49 @@ ClientesRouter.post("/", async (req, res) => {
   }
 });
 
-// ClientesRouter.get("/:id", async (req, res) => {
-//   try {
-//     const cliente = await ClientesController.listarUmItemPorId(req);
-//     if (cliente != null) {
-//       res.status(200).json({ cliente });
-//     } else {
-//       throw new Error(
-//         "Comanda não encontrada. Verifique se o ID está correto e tente novamente."
-//       );
-//     }
-//   } catch (e) {
-//     res.status(400).json({ erro: e.message });
-//   }
-// });
+ClientesRouter.get("/:id", async (req, res) => {
+  try {
+    const cliente = await ClientesController.listarUmItemPorId(req);
+    if (cliente != null) {
+      res.status(200).json({ cliente });
+    } else {
+      throw new Error(
+        "Comanda não encontrada. Verifique se o ID está correto e tente novamente."
+      );
+    }
+  } catch (e) {
+    res.status(400).json({ erro: e.message });
+  }
+});
 
-// ClientesRouter.get("/", async (req, res) => {
-//   try {
-//     const cliente = await ClientesController.listar();
-//     console.log(req.params);
-//     res.status(200).json({ cliente });
-//   } catch (e) {
-//     res.status(400).json({ erro: e.message });
-//   }
-// });
+ClientesRouter.get("/", async (req, res) => {
+  try {
+    const cliente = await ClientesController.listar();
+    console.log(req.params);
+    res.status(200).json({ cliente });
+  } catch (e) {
+    res.status(400).json({ erro: e.message });
+  }
+});
 
-// ClientesRouter.delete("/:id", async (req, res) => {
-//   try {
-//     const deletado = await ClientesController.deletar(req.params.id);
-//     const cliente = await ClientesController.listar();
-//     res.status(200).json({ cliente });
-//   } catch (e) {
-//     res.status(400).json({ erro: e.message });
-//   }
-// });
+ClientesRouter.delete("/:id", async (req, res) => {
+  try {
+    const deletado = await ClientesController.deletar(req.params.id);
+    const cliente = await ClientesController.listar();
+    res.status(200).json({ cliente });
+  } catch (e) {
+    res.status(400).json({ erro: e.message });
+  }
+});
 
-// ClientesRouter.patch("/:id", async (req, res) => {
-//   try {
-//     const modificado = await ClientesController.update(req);
-//     const cliente = await ClientesController.listar();
-//     res.status(200).json({ cliente });
-//   } catch (e) {
-//     res.status(400).json({ erro: e.message });
-//   }
-// });
+ClientesRouter.patch("/:id", async (req, res) => {
+  try {
+    const modificado = await ClientesController.update(req);
+    const cliente = await ClientesController.listar();
+    res.status(200).json({ cliente });
+  } catch (e) {
+    res.status(400).json({ erro: e.message });
+  }
+});
 
 export default ClientesRouter;
