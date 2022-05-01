97aa1ec1f967b45c6106523f4d1a02c0c6832c11
Ytallo Bruno <ytallobruno@hotmail.com>
2022-05-01 11:52:07 -0300

    feat: consertado erros validador

--- src/utils/Clientes/Validador.js
+++ src/utils/Clientes/Validador.js
@@ -1,7 +1,7 @@
 class Validador {
   static checaEmail(email) {
     let RegExEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if (email.test(RegExEmail)) {
+    if (RegExEmail.test(email)) {
       return true;
     } else {
       return false;
