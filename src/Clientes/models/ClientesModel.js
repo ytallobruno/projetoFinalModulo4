a1c3a4af89825e217fd20583d01382c5e6a9a977
Ytallo Bruno <ytallobruno@hotmail.com>
2022-05-01 13:20:01 -0300

    feat: corrigido erros

--- src/Clientes/models/ClientesModel.js
+++ src/Clientes/models/ClientesModel.js
@@ -1,5 +1,5 @@
 class ClientesModel {
-  constructor({ id_comanda, id_pedido, nome_cliente, email_cliente }) {
+  constructor( id_comanda, id_pedido, nome_cliente, email_cliente ) {
     this.id_comanda = id_comanda;
     this.id_pedido = id_pedido;
     this.nome_cliente = nome_cliente;
