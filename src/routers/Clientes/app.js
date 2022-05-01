00691b31256a21a9343df2e9e2cf0c51494c51fe
Ytallo Bruno <ytallobruno@hotmail.com>
2022-05-01 11:57:00 -0300

    feat: consertado erro post

--- src/routers/Clientes/app.js
+++ src/routers/Clientes/app.js
@@ -7,62 +7,62 @@ const ClientesRouter = express.Router();
 
 ClientesRouter.post("/", async (req, res) => {
   try {
-    if (Validador.checaEmail(req.body.validade)) {
+    if (Validador.checaEmail(req.body.email_cliente)) {
       const dados = req.body
       const cliente = new ClientesModel(dados)
       await ClientesController.adicionar(cliente)
       res.status(201).json({cliente})
     } else {
-      throw new Error("E-mail inválido! Ensina um no seguinte formato: usuario@dominio.com");
+      throw new Error("E-mail inválido! Insira e-mail um no seguinte formato: usuario@dominio.com");
     }
   } catch (e) {
     res.status(400).json({ erro: e.message });
   }
 });
 
-ClientesRouter.get("/:id", async (req, res) => {
-  try {
-    const cliente = await ClientesController.listarUmItemPorId(req);
-    if (cliente != null) {
-      res.status(200).json({ cliente });
-    } else {
-      throw new Error(
-        "Comanda não encontrada. Verifique se o ID está correto e tente novamente."
-      );
-    }
-  } catch (e) {
-    res.status(400).json({ erro: e.message });
-  }
-});
+// ClientesRouter.get("/:id", async (req, res) => {
+//   try {
+//     const cliente = await ClientesController.listarUmItemPorId(req);
+//     if (cliente != null) {
+//       res.status(200).json({ cliente });
+//     } else {
+//       throw new Error(
+//         "Comanda não encontrada. Verifique se o ID está correto e tente novamente."
+//       );
+//     }
+//   } catch (e) {
+//     res.status(400).json({ erro: e.message });
+//   }
+// });
 
-ClientesRouter.get("/", async (req, res) => {
-  try {
-    const cliente = await ClientesController.listar();
-    console.log(req.params);
-    res.status(200).json({ cliente });
-  } catch (e) {
-    res.status(400).json({ erro: e.message });
-  }
-});
+// ClientesRouter.get("/", async (req, res) => {
+//   try {
+//     const cliente = await ClientesController.listar();
+//     console.log(req.params);
+//     res.status(200).json({ cliente });
+//   } catch (e) {
+//     res.status(400).json({ erro: e.message });
+//   }
+// });
 
-ClientesRouter.delete("/:id", async (req, res) => {
-  try {
-    const deletado = await ClientesController.deletar(req.params.id);
-    const cliente = await ClientesController.listar();
-    res.status(200).json({ cliente });
-  } catch (e) {
-    res.status(400).json({ erro: e.message });
-  }
-});
+// ClientesRouter.delete("/:id", async (req, res) => {
+//   try {
+//     const deletado = await ClientesController.deletar(req.params.id);
+//     const cliente = await ClientesController.listar();
+//     res.status(200).json({ cliente });
+//   } catch (e) {
+//     res.status(400).json({ erro: e.message });
+//   }
+// });
 
-ClientesRouter.patch("/:id", async (req, res) => {
-  try {
-    const modificado = await ClientesController.update(req);
-    const cliente = await ClientesController.listar();
-    res.status(200).json({ cliente });
-  } catch (e) {
-    res.status(400).json({ erro: e.message });
-  }
-});
+// ClientesRouter.patch("/:id", async (req, res) => {
+//   try {
+//     const modificado = await ClientesController.update(req);
+//     const cliente = await ClientesController.listar();
+//     res.status(200).json({ cliente });
+//   } catch (e) {
+//     res.status(400).json({ erro: e.message });
+//   }
+// });
 
 export default ClientesRouter;
